75f981a4c49e8c86f616d4a7d205f119b83c0a33
tarobins <tarobins@gmail.com>
2014-08-21 20:32:14 -0400

    add file replacing serializer and retriever tests

--- src/ca/tomrobinson/serialization/test/FileReplacingSerializerAndRetrieverTest.java
+++ src/ca/tomrobinson/serialization/test/FileReplacingSerializerAndRetrieverTest.java
@@ -12,47 +12,60 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import ca.tomrobinson.serialization.FileReplacingRetriever;
+import ca.tomrobinson.serialization.FileReplacingSerializer;
 import ca.tomrobinson.serialization.factories.FileBasedObjectStreamFactory;
+import ca.tomrobinson.serialization.factories.FileBasedObjectStreamFactoryImpl;
 
 public class FileReplacingSerializerAndRetrieverTest {
 
-	private ObjectOutputStream _outputStream;
-	private ObjectInputStream _inputStream;
+	private static final String FILENAME = "TestFile";
+	
+	File _file;
+	FileBasedObjectStreamFactory _fileFactory;
+	FileReplacingRetriever<Integer> _retriever;
+	FileReplacingSerializer<Integer> _serializer;
 	
 	@Before
 	public void setUp() throws Exception {
+		_file = new File(FILENAME);
 		
-		PipedInputStream pipedInputStream = new PipedInputStream();
-		PipedOutputStream pipedOutputStream = new PipedOutputStream(pipedInputStream);
-		
-		_outputStream = new ObjectOutputStream(pipedOutputStream);
-		_inputStream = new ObjectInputStream(pipedInputStream);
-		
+		_fileFactory = new FileBasedObjectStreamFactoryImpl();
+		_retriever = new FileReplacingRetriever<>(_file, _fileFactory);
+		_serializer = new FileReplacingSerializer<>(_file, _fileFactory);
 	}
 	
-	private class MockStreamFactory implements FileBasedObjectStreamFactory {
-
-		@Override
-		public ObjectOutputStream getOutputStream(File file) {
-			return _outputStream;
-		}
-
-		@Override
-		public ObjectInputStream getInputStream(File file) {
-			return _inputStream;
-		}
-		
-	}
 	
 	@After
 	public void tearDown() throws Exception {
+		if (_file.exists()) {
+			_file.delete();
+		}
 	}
 
 	@Test
 	public void testSerializeAndRetrieve() {
-
+		Integer testInteger = new Integer(20);
+		_serializer.serialize(testInteger);
+		Integer retrievedInteger = _retriever.retrieve();
+		
+		assertEquals(testInteger, retrievedInteger);
+	}
+	
+	@Test
+	public void testSerializeReplaceAndRetrieve() {
+		Integer testInteger1 = new Integer(20);
+		Integer testInteger2 = new Integer(40);
+		_serializer.serialize(testInteger1);
+		Integer retrievedInteger = _retriever.retrieve();
+		
+		assertEquals(testInteger1, retrievedInteger);
+	
+		_serializer.serialize(testInteger2);
+		retrievedInteger = _retriever.retrieve();
+		
+		assertEquals(testInteger2, retrievedInteger);
 		
 		
 	}
-
 }
