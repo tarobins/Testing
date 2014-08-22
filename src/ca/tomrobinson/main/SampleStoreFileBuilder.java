03a3bd36d6609979b72fa7fe20545a5ed36be973
tarobins <tarobins@gmail.com>
2014-08-22 13:31:31 -0400

    Start store builder

---
+++ src/ca/tomrobinson/main/SampleStoreFileBuilder.java
@@ -0,0 +1,20 @@
+package ca.tomrobinson.main;
+
+import java.io.Serializable;
+
+import ca.tomrobinson.contacts.Contact;
+import ca.tomrobinson.serialization.FileReplacingSerializer;
+import ca.tomrobinson.store.SerializableContactStore;
+
+public class SampleStoreFileBuilder {
+
+	public static void main(String[] args) {
+		
+		FileReplacingSerializer<SerializableContactStore> serializer = 
+			new 
+		
+		SerializableContactStore contactStore
+
+	}
+
+}
